2f7104e50e1d709ef59739ee5a5848796b54e8bd
Frah01 <vinario.francesco@gmail.com>
2022-12-15 15:29:17 +0100

    Creato ciclo

--- main.js
+++ main.js
@@ -55,3 +55,44 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+let itemsContent
+
+posts.forEach((elem)=>{
+    itemsContent +=
+` <div class="post">
+<div class="post__header">
+    <div class="post-meta">                    
+        <div class="post-meta__icon">
+            <img class="profile-pic" src=${elem.author.image} alt="Phil Mangione">                    
+        </div>
+        <div class="post-meta__data">
+            <div class="post-meta__author">Phil Mangione</div>
+            <div class="post-meta__time">4 mesi fa</div>
+        </div>                    
+    </div>
+</div>
+<div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+<div class="post__image">
+    <img src=${elem.media} alt="">
+</div>
+<div class="post__footer">
+    <div class="likes js-likes">
+        <div class="likes__cta">
+            <a class="like-button  js-like-button" href="#" data-postid="1">
+                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                <span class="like-button__label">Mi Piace</span>
+            </a>
+        </div>
+        <div class="likes__counter">
+            Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+        </div>
+    </div> 
+</div>            
+</div>`
+})
+
+
+const itemContainer = document.getElementById('container')
+itemContainer.innerHTML += itemsContent;
+
